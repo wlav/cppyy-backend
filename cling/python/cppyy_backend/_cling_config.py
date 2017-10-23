63aa764ecf3ffe5f0e726d1f32da14e22165caf3
Shaheed Haque <srhaque@theiet.org>
2017-10-23 11:04:26 +0100

    Use os-independent path construction.

--- cling/python/cppyy_backend/_cling_config.py
+++ cling/python/cppyy_backend/_cling_config.py
@@ -4,10 +4,9 @@ import os, sys, subprocess
 MYHOME = os.path.dirname(__file__)
 
 def main():
-    if len(sys.argv) == 2 and \
-           (sys.argv[1] == '--cflags' or sys.argv[1] == '--cppflags'):
-        print('-I%s/include' % (MYHOME,))
-        return 0
-    else:
-        print('Usage: cling-config [--cflags] [--cppflags]')
+    if len(sys.argv) == 2:
+        if sys.argv[1] == '--cflags' or sys.argv[1] == '--cppflags':
+            print('-I' + os.path.join(MYHOME, "include"))
+            return 0
+    print('Usage: cling-config [--cflags] [--cppflags]')
     return 1
