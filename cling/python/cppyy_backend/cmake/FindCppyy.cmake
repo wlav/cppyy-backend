78a8c8b9531d149e83898b2ac71cac26effe35ed
Shaheed Haque <srhaque@theiet.org>
2017-10-24 15:01:28 +0100

    Make CMake support independent of cling-config.

--- cling/python/cppyy_backend/cmake/FindCppyy.cmake
+++ cling/python/cppyy_backend/cmake/FindCppyy.cmake
@@ -11,7 +11,6 @@
 #   Cppyy_FOUND - set to true if Cppyy is found
 #   Cppyy_DIR - the directory where Cppyy is installed
 #   Cppyy_EXECUTABLE - the path to the Cppyy executable
-#   CppyyConfig_EXECUTABLE - the path to the Cppyy config executable
 #   Cppyy_INCLUDE_DIRS - Where to find the ROOT header files.
 #   Cppyy_VERSION - the version number of the Cppyy backend.
 #
@@ -19,40 +18,24 @@
 # The minimum required version of Cppyy can be specified using the
 # standard syntax, e.g.  find_package(Cppyy 4.19)
 #
-# All information is collected from the CppyyConfig_EXECUTABLE so the
-# version to be found can be changed from the command line by means of
-# setting CppyyConfig_EXECUTABLE.
 
 find_program(Cppyy_EXECUTABLE NAMES rootcling)
-find_program(CppyyConfig_EXECUTABLE NAMES cling-config)
 
-if(Cppyy_EXECUTABLE AND CppyyConfig_EXECUTABLE)
+if(Cppyy_EXECUTABLE)
   #
   # Cppyy_DIR.
   #
-  execute_process(
-    COMMAND ${CppyyConfig_EXECUTABLE} --cmake
-    RESULT_VARIABLE _STATUS
-    OUTPUT_VARIABLE _STDOUT
-    ERROR_VARIABLE _STDERR
-    OUTPUT_STRIP_TRAILING_WHITESPACE)
-  set(Cppyy_DIR ${_STDOUT})
+  set(Cppyy_DIR ${CMAKE_CURRENT_LIST_DIR})
   #
   # Cppyy_INCLUDE_DIRS.
   #
-  execute_process(
-    COMMAND ${CppyyConfig_EXECUTABLE} --cppflags
-    RESULT_VARIABLE _STATUS
-    OUTPUT_VARIABLE _STDOUT
-    ERROR_VARIABLE _STDERR
-    OUTPUT_STRIP_TRAILING_WHITESPACE)
-  string(REGEX REPLACE "^-I" "" _STDOUT "${_STDOUT}")
-  set(Cppyy_INCLUDE_DIRS ${_STDOUT})
+  get_filename_component(Cppyy_INCLUDE_DIRS ${Cppyy_DIR} DIRECTORY)
+  set(Cppyy_INCLUDE_DIRS "${Cppyy_INCLUDE_DIRS}/include")
+  #
+  # Cppyy_VERSION.
+  #
   find_package(ROOT QUIET REQUIRED PATHS ${CMAKE_CURRENT_LIST_DIR})
   if(ROOT_FOUND)
-    #
-    # Cppyy_VERSION.
-    #
     set(Cppyy_VERSION ${ROOT_VERSION})
   endif()
 endif()
@@ -60,7 +43,7 @@ endif()
 include(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(
     Cppyy
-    REQUIRED_VARS Cppyy_EXECUTABLE CppyyConfig_EXECUTABLE Cppyy_DIR Cppyy_INCLUDE_DIRS
+    REQUIRED_VARS Cppyy_EXECUTABLE Cppyy_DIR Cppyy_INCLUDE_DIRS
     VERSION_VAR Cppyy_VERSION)
 
 mark_as_advanced(Cppyy_VERSION)
