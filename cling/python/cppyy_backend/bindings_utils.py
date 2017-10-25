7a12edfefc5f849d39dc69bee4e0243676c0ebaf
Shaheed Haque <srhaque@theiet.org>
2017-10-25 18:48:40 +0100

    An experimental discovery mechanism, using the .rootmap to construct a Python package named after pkg_lib, and usable via "import pkg_lib".

--- cling/python/cppyy_backend/bindings_utils.py
+++ cling/python/cppyy_backend/bindings_utils.py
@@ -7,6 +7,72 @@ import setuptools
 import cppyy
 
 
+def rootmapper(pkg_dir, pkg_lib, cmake_shared_library_prefix, cmake_shared_library_suffix, pkg_module):
+    """
+    Populate a module with some rootmap'ped bindings.
+
+    :param pkg_dir:         Base directory of the bindings.
+    :param pkg_lib:         Name of the bindings.
+    :param cmake_shared_library_prefix:
+                            ${cmake_shared_library_prefix}
+    :param cmake_shared_library_suffix:
+                            ${cmake_shared_library_suffix}
+    :param pkg_module:      The module to be populated.
+    """
+    pkg_file = cmake_shared_library_prefix + pkg_lib + cmake_shared_library_suffix
+    #
+    # Load the library.
+    #
+    cppyy.load_reflection_info(os.path.join(pkg_dir, pkg_file))
+    #
+    # Parse the rootmap file.
+    #
+    namespaces = {}
+    objects = {}
+    with open(os.path.join(pkg_dir, pkg_lib + '.rootmap'), 'rU') as rootmap:
+        for line in rootmap:
+            if line.startswith('['):
+                #
+                # We found the start of the section we care about.
+                #
+                break
+        for line in rootmap:
+            line = line.strip().split(None, 1)
+            if len(line) < 2:
+                continue
+            keyword, name = line
+            simplenames = tuple(name.split('::'))
+            if keyword == 'namespace':
+                namespaces[simplenames] = cppyy._backend.LookupCppEntity(name)
+            elif keyword in ('class', 'var'):
+                #
+                # Classes, variables etc.
+                #
+                objects[simplenames] = cppyy._backend.LookupCppEntity(name)
+        #
+        # Set up namespaces, then other objects, in depth order.
+        #
+        for entities in [namespaces, objects]:
+            levels = [len(k) for k in entities.keys()]
+            levels.sort()
+            for level in levels:
+                names_at_level = [k for k in entities.keys() if len(k) == level]
+                for simplenames in names_at_level:
+                    parent = pkg_module
+                    for prefix in simplenames[:-1]:
+                        try:
+                            parent = getattr(parent, prefix)
+                        except AttributeError:
+                            parent = parent[prefix]
+                    entity = entities[simplenames]
+                    #
+                    # Properties can simply be added to the parent, classes however can only have one parent.
+                    #
+                    if isinstance(entity, cppyy.gbl.PropertyProxy):
+                        setattr(parent, simplenames[-1], entity)
+                    else:
+                        setattr(parent, simplenames[-1], type(simplenames[-1], (entity,), {}))
+
 def setup(pkg_dir, pkg_lib, cmake_shared_library_prefix, cmake_shared_library_suffix, pkg_version):
     """
     Wrap setuptools.setup for some bindings.
