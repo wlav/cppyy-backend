195db42da88da402081f27ecbd2e1e8d2757e533
Wim Lavrijsen <WLavrijsen@lbl.gov>
2018-01-11 12:04:34 -0800

    handle templated decls that depend on template parameters to be meaningful

--- cling/create_src_directory.py
+++ cling/create_src_directory.py
@@ -340,5 +340,6 @@ os.system('patch -p1 < patches/scanner.diff')
 os.system('patch -p1 < patches/scanner_2.diff')
 os.system('patch -p1 < patches/faux_typedef.diff')
 os.system('patch -p1 < patches/template_fwd.diff')
+os.system('patch -p1 < patches/dep_template.diff')
 
 # done!
