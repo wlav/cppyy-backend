ce9e7a84cb72aaffc249fefaf674ba344358a34a
Wim Lavrijsen <WLavrijsen@lbl.gov>
2018-07-08 22:19:50 -0700

    first load dependencies w/o full path, before trying full path

--- cling/python/cppyy_backend/loader.py
+++ cling/python/cppyy_backend/loader.py
@@ -11,7 +11,10 @@ def load_cpp_backend():
       # failed ... load dependencies explicitly
         libpath = os.path.join(os.path.dirname(__file__), 'lib')
         for dep in ['libCore.so', 'libThread.so', 'libRIO.so', 'libCling.so']:
-            ctypes.CDLL(os.path.join(libpath, dep), ctypes.RTLD_GLOBAL)
+            fpath = os.path.join(libpath, dep)
+            if os.path.exists(fpath):
+                dep = fpath
+            ctypes.CDLL(dep, ctypes.RTLD_GLOBAL)
         c = ctypes.CDLL(os.path.join(libpath, 'libcppyy_backend.so'), ctypes.RTLD_GLOBAL)
 
     return c
