fe66b0f0ee0fa67fcb3ce69f8072450b8453918b
Wim Lavrijsen <WLavrijsen@lbl.gov>
2018-12-04 15:01:03 -0800

    fix extension for windows

--- clingwrapper/python/cppyy_backend/loader.py
+++ clingwrapper/python/cppyy_backend/loader.py
@@ -1,20 +1,25 @@
 """ cppyy_backend loader
 """
 
-import os, ctypes
+import os, sys, ctypes
+
+if 'win' in sys.platform:
+   soext = '.dll'
+else:
+   soext = '.so'
 
 def load_cpp_backend():
     try:
       # normal load, allowing for user overrides of LD_LIBRARY_PATH
-        c = ctypes.CDLL("libcppyy_backend.so", ctypes.RTLD_GLOBAL)
+        c = ctypes.CDLL('libcppyy_backend'+soext, ctypes.RTLD_GLOBAL)
     except OSError:
       # failed ... load dependencies explicitly
         libpath = os.path.join(os.path.dirname(__file__), 'lib')
-        for dep in ['libCore.so', 'libThread.so', 'libRIO.so', 'libCling.so']:
-            fpath = os.path.join(libpath, dep)
+        for dep in ['libCore', 'libThread', 'libRIO', 'libCling']:
+            fpath = os.path.join(libpath, dep+soext)
             if os.path.exists(fpath):
                 dep = fpath
             ctypes.CDLL(dep, ctypes.RTLD_GLOBAL)
-        c = ctypes.CDLL(os.path.join(libpath, 'libcppyy_backend.so'), ctypes.RTLD_GLOBAL)
+        c = ctypes.CDLL(os.path.join(libpath, 'libcppyy_backend'+soext), ctypes.RTLD_GLOBAL)
 
     return c
