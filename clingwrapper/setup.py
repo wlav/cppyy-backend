c275d91a3be832376f25e46dcf4a830ffff3654e
Wim Lavrijsen <WLavrijsen@lbl.gov>
2020-08-20 22:38:38 -0700

    use pyproject.toml to force build order for wheels

--- clingwrapper/setup.py
+++ clingwrapper/setup.py
@@ -2,7 +2,6 @@ import codecs, glob, os, sys, subprocess
 from setuptools import setup, find_packages, Extension
 from distutils import log
 
-from setuptools.dist import Distribution
 from setuptools.command.install import install as _install
 from distutils.command.build_ext import build_ext as _build_ext
 from distutils.command.clean import clean as _clean
@@ -13,11 +12,6 @@ try:
 except ImportError:
     has_wheel = False
 from distutils.errors import DistutilsSetupError
-
-force_bdist = False
-if '--force-bdist' in sys.argv:
-    force_bdist = True
-    sys.argv.remove('--force-bdist')
 
 requirements = ['cppyy-cling<6.22', 'cppyy-cling>=6.21']
 setup_requirements = ['wheel']
@@ -165,8 +159,9 @@ class my_install(_install):
 
 cmdclass = {
         'build_ext': my_build_cpplib,
-        'clean': my_clean,
+        #'clean': my_clean,
         'install': my_install }
+
 if has_wheel:
     class my_bdist_wheel(_bdist_wheel):
         def finalize_options(self):
@@ -178,29 +173,6 @@ if has_wheel:
             _bdist_wheel.finalize_options(self)
             self.root_is_pure = True
     cmdclass['bdist_wheel'] = my_bdist_wheel
-
-
-#
-# customized distribition to disable binaries
-#
-class MyDistribution(Distribution):
-    def run_commands(self):
-        # pip does not resolve dependencies before building binaries, so unless
-        # packages are installed one-by-one, on old install is used or the build
-        # will simply fail hard. The following is not completely quiet, but at
-        # least a lot less conspicuous.
-        if not is_manylinux() and not force_bdist:
-            disabled = set((
-                'bdist_wheel', 'bdist_egg', 'bdist_wininst', 'bdist_rpm'))
-            for cmd in self.commands:
-                if not cmd in disabled:
-                    self.run_command(cmd)
-                else:
-                    log.info('Command "%s" is disabled', cmd)
-                    cmd_obj = self.get_command_obj(cmd)
-                    cmd_obj.get_outputs = lambda: None
-        else:
-            return Distribution.run_commands(self)
 
 
 setup(
@@ -247,7 +219,6 @@ setup(
         sources=glob.glob(os.path.join('src', 'clingwrapper.cxx')))],
 
     cmdclass=cmdclass,
-    distclass=MyDistribution,
 
     zip_safe=False,
 )
