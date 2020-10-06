f07861b8bdc949f42d592c2d94b0a95ac608b686
Wim Lavrijsen <WLavrijsen@lbl.gov>
2020-10-06 12:42:35 -0700

    up required version of cppyy-cling

--- clingwrapper/setup.py
+++ clingwrapper/setup.py
@@ -13,7 +13,7 @@ except ImportError:
     has_wheel = False
 from distutils.errors import DistutilsSetupError
 
-requirements = ['cppyy-cling>=6.21.3']
+requirements = ['cppyy-cling>=6.21.4']
 setup_requirements = ['wheel']
 if 'build' in sys.argv or 'install' in sys.argv:
     setup_requirements += requirements
