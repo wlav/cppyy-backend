08a6b9b38ec90de0f22a3647c573b4dfc62eece8
Wim Lavrijsen <WLavrijsen@lbl.gov>
2021-01-05 18:52:28 +0000

    improve chances of finding the backend library, given the multitude of install options ...

--- cling/python/cppyy_backend/loader.py
+++ cling/python/cppyy_backend/loader.py
@@ -16,14 +16,17 @@ else:
     soext = '.so'
 
 soabi = sysconfig.get_config_var("SOABI")
+soext2 = sysconfig.get_config_var("SO")
 
 
 def _load_helper(bkname):
+    errors = set()
+
  # normal load, allowing for user overrides of LD_LIBRARY_PATH
     try:
         return ctypes.CDLL(bkname, ctypes.RTLD_GLOBAL)
-    except OSError:
-         pass
+    except OSError as e:
+        errors.add(str(e))
 
  # failed ... load dependencies explicitly
     try:
@@ -40,11 +43,11 @@ def _load_helper(bkname):
                     if dep == 'libCling': ldtype = ctypes.RTLD_LOCAL
                     ctypes.CDLL(fpath, ldtype)
                     break
-        return ctypes.CDLL(os.path.join(pkgpath, 'lib', bkname), ctypes.RTLD_GLOBAL)
-    except OSError:
-        pass
+        return ctypes.CDLL(os.path.join(pkgpath, 'lib', bkname), ctypes.RTLD_GLOBAL), errors
+    except OSError as e:
+        errors.add(str(e))
 
-    return None
+    return None, errors
 
 
 _precompiled_header_ensured = False
@@ -56,22 +59,29 @@ def load_cpp_backend():
      # distribution as there are too many varieties; create it now if needed
         ensure_precompiled_header()
 
-    altbkname = None
+    names = list()
     try:
         bkname = os.environ['CPPYY_BACKEND_LIBRARY']
         if bkname.rfind(soext) < 0:
             bkname += soext
+        names.append(bkname)
     except KeyError:
-        bkname = 'libcppyy_backend'+soext
+        names.append('libcppyy_backend'+soext)
         if soabi:
-            altbkname = 'libcppyy_backend.'+soabi+soext
+            names.append('libcppyy_backend.'+soabi+soext)
+        if soext2:
+            names.append('libcppyy_backend'+soext2)
 
-    c = _load_helper(bkname)
-    if not c and altbkname is not None:
-        c = _load_helper(altbkname)
+    err = set()
+    for name in names:
+        c, err2 = _load_helper(name)
+        if c:
+            break
+        err = err.union(err2)
 
     if not c:
-        raise RuntimeError("could not load cppyy_backend library")
+        raise RuntimeError("could not load cppyy_backend library, details:\n%s" %
+            '\n'.join(['  '+x for x in err]))
 
     return c
 
