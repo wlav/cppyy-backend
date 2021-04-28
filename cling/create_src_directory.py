5805aa108403bc4d7c575d566d5f81f932037ac2
Wim Lavrijsen <WLavrijsen@lbl.gov>
2021-04-27 18:06:53 -0700

    add luhelper.diff to list of patch files

--- cling/create_src_directory.py
+++ cling/create_src_directory.py
@@ -111,7 +111,7 @@ except ImportError:
 
 
 for fdiff in ('typedef_of_private', 'optlevel2_forced', 'explicit_template', 'alias_template',
-              'incomplete_types', 'clang_printing', 'improv_load',
+              'incomplete_types', 'clang_printing', 'luhelper', 'improv_load',
               'pch', 'win64rtti', 'win64', 'win64s2', 'locales', 'gcc10'):
     fpatch = os.path.join('patches', fdiff+'.diff')
     print(' ==> applying patch:', fpatch)
