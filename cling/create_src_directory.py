7606d831967a093e730cc848a69cee4267b781fd
Wim Lavrijsen <WLavrijsen@lbl.gov>
2021-05-08 21:58:16 -0700

    cmake fix for constructing custom CLING target

--- cling/create_src_directory.py
+++ cling/create_src_directory.py
@@ -111,7 +111,7 @@ except ImportError:
 
 for fdiff in ('typedef_of_private', 'optlevel2_forced', 'explicit_template', 'alias_template',
               'incomplete_types', 'clang_printing', 'luhelper', 'improv_load',
-              'pch', 'win64rtti', 'win64', 'win64s2', 'locales'):
+              'pch', 'win64rtti', 'win64', 'win64s2', 'locales', 'build'):
     fpatch = os.path.join('patches', fdiff+'.diff')
     print(' ==> applying patch:', fpatch)
     pset = patch.fromfile(fpatch)
