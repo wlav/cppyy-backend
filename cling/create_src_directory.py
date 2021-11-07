fab502b5176febaa92fe488c363efc43bbaa8347
Wim Lavrijsen <WLavrijsen@lbl.gov>
2021-11-06 18:50:06 -0700

    add all gnu system dirs

--- cling/create_src_directory.py
+++ cling/create_src_directory.py
@@ -111,7 +111,7 @@ except ImportError:
 
 for fdiff in ('typedef_of_private', 'optlevel2_forced', 'explicit_template', 'alias_template',
               'incomplete_types', 'clang_printing', 'luhelper', 'improv_load', 'unload',
-              'pch', 'win64rtti', 'win64', 'win64s2', 'locales', 'build'):
+              'pch', 'win64rtti', 'win64', 'win64s2', 'locales', 'build', 'system_dirs'):
     fpatch = os.path.join('patches', fdiff+'.diff')
     print(' ==> applying patch:', fpatch)
     pset = patch.fromfile(fpatch)
