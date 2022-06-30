4a9422e5d853399b0461484f0c98824cf49d499b
Wim Lavrijsen <WLavrijsen@lbl.gov>
2022-06-29 21:59:20 -0700

    up versions for release of cppyy-backend 1.14.9

--- clingwrapper/setup.py
+++ clingwrapper/setup.py
@@ -13,7 +13,7 @@ except ImportError:
     has_wheel = False
 from distutils.errors import DistutilsSetupError
 
-requirements = ['cppyy-cling==6.25.4']
+requirements = ['cppyy-cling==6.27.0']
 setup_requirements = ['wheel']
 if 'build' in sys.argv or 'install' in sys.argv:
     setup_requirements += requirements
@@ -189,7 +189,7 @@ setup(
     author='Wim Lavrijsen',
     author_email='WLavrijsen@lbl.gov',
 
-    version='1.14.8',
+    version='1.14.9',
 
     license='LBNL BSD',
 
