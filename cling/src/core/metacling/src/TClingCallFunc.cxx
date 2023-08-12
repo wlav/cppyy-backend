00c7bed66b01c79e803ae7a41edd08d3273f1ca5
Wim Lavrijsen <WLavrijsen@lbl.gov>
2023-08-11 19:14:08 -0700

    upgrade Cling from upstream (28.04)

--- cling/src/core/metacling/src/TClingCallFunc.cxx
+++ cling/src/core/metacling/src/TClingCallFunc.cxx
@@ -917,6 +917,7 @@ int TClingCallFunc::get_wrapper_code(std::string &wrapper_name, std::string &wra
    ostringstream buf;
    buf << "#pragma clang diagnostic push\n"
           "#pragma clang diagnostic ignored \"-Wformat-security\"\n"
+          "#pragma clang diagnostic ignored \"-Wunused-value\"\n"
           "__attribute__((used)) "
           "extern \"C\" void ";
    buf << wrapper_name;
