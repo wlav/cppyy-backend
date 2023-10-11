164030f7d769814107d8e3cebce60ecaf8ae5ef7
Wim Lavrijsen <WLavrijsen@lbl.gov>
2023-10-11 10:47:00 -0700

    more debugging printout for debugging problem with job number

--- circleci.py
+++ circleci.py
@@ -103,6 +103,7 @@ def start_job(
     )
 
     res = conn.getresponse()
+    print("conn response:\n", res.read().decode("utf-8"))
     job_data = json.loads(res.read().decode("utf-8"))
 
     print(job_data)
