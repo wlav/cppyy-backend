a8e3a52e7f64c85f143f525aa11d0887f8d528b2
Wim Lavrijsen <WLavrijsen@lbl.gov>
2023-10-12 15:58:16 -0700

    drop cross-compiled platforms from github CI (can't work)

--- cling/setup.py
+++ cling/setup.py
@@ -162,8 +162,8 @@ class my_cmake_build(_build):
                 CMAKE_COMMAND += ['-Thost=x86', '-DCMAKE_GENERATOR_PLATFORM=win32']
         elif 'darwin' in sys.platform:
             import platform
-            if 'arm64' in platform.machine() or 'arm64' in os.getenv("CLING_CMAKE_BUILD_TARGET"):
-                CMAKE_COMMAND += ['-DCMAKE_OSX_ARCHITECTURES=arm64', '-DLLVM_TARGETS_TO_BUILD=ARM;AArch64;NVPTX']
+            if 'arm64' in platform.machine():
+                CMAKE_COMMAND += ['-DLLVM_TARGETS_TO_BUILD=ARM;AArch64;NVPTX']
         CMAKE_COMMAND.append('-DCMAKE_INSTALL_PREFIX='+prefix)
 
         log.info('Running cmake for cppyy-cling: %s', ' '.join(CMAKE_COMMAND))
